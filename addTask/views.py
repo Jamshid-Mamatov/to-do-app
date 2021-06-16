18fcc8c5367fa5d76c4038995bffea287abffb9a
Jamshid <mamatovjamshid506@gmail.com>
2021-06-16 13:53:05 +0500

    added task

--- addTask/views.py
+++ addTask/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
+information={}
+information['tasks']=[]
 
-# Create your views here.
+def home(request):
+    home_page='''
+    <h1 style= color:red;text-align:center> added task </h1>
+    '''
+    information['tasks'].append("ishi qii")    
+
+    print(information)
+    inform=json.dumps(information)
+    with open("sample.json", "w") as outfile:
+        outfile.write(inform)
+
+    return HttpResponse(home_page)
+
+
+
